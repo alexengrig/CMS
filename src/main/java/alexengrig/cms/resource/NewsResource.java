1177c5534d6f166cebe1bc56299f55cb47f06d4b
alexengrig <alexengrigdev@gmail.com>
2018-12-09 23:17:18 +0300

    Create controller of news resource

---
+++ src/main/java/alexengrig/cms/resource/NewsResource.java
@@ -0,0 +1,42 @@
+package alexengrig.cms.resource;
+
+import alexengrig.cms.model.News;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/news")
+public class NewsResource {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<News> findOne(@PathVariable("id") News newsFromDb) {
+        return ResponseEntity.ok(newsFromDb);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<News>> findAll() {
+        return ResponseEntity.ok(Arrays.asList(new News(), new News()));
+    }
+
+    @PostMapping
+    public ResponseEntity<News> newNews(@RequestBody News news) {
+        return new ResponseEntity<>(news, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<News> updateNews(@PathVariable("id") News newsFromDb,
+                                           @RequestBody News news) {
+        return ResponseEntity.ok(news);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeNews(@PathVariable("id") News newsFromDb) {
+
+    }
+
+}
