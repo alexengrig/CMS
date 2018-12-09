99bb60edd13e7591b980d2e297585d7e820593b5
alexengrig <alexengrigdev@gmail.com>
2018-12-09 22:51:37 +0300

    Create model of review

---
+++ src/main/java/alexengrig/cms/model/Review.java
@@ -0,0 +1,16 @@
+package alexengrig.cms.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Review {
+
+    private User reviewer;
+
+    private String status;
+
+}
