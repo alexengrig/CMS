6db303044861b708218bf1e7ef2090df1c800d82
alexengrig <alexengrigdev@gmail.com>
2018-12-12 00:25:21 +0300

    Set entity of category

--- src/main/java/alexengrig/cms/model/Category.java
+++ src/main/java/alexengrig/cms/model/Category.java
@@ -2,9 +2,15 @@ package alexengrig.cms.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "category")
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String name;
