4a9932ed3931a88e1124e3fd36883e7463f83b8b
alexengrig <alexengrigdev@gmail.com>
2018-12-12 00:25:50 +0300

    Set entity of user

--- src/main/java/alexengrig/cms/model/User.java
+++ src/main/java/alexengrig/cms/model/User.java
@@ -2,9 +2,15 @@ package alexengrig.cms.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "\"user\"")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String identity;
